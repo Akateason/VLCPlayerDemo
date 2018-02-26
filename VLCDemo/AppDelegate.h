257db903bc9a2570af303603bd20f2ac86bcff63
teason <teason.xie@cootek.cn>
2018-02-26 16:31:49 +0800

    Initial Commit

---
+++ VLCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VLCDemo
+//
+//  Created by teason23 on 2018/2/26.
+//  Copyright © 2018年 teason23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
