2eac8b859c8c18e6b02f48279f1bc46be6a85450
teason23 <akateason@qq.com>
2018-02-28 01:44:51 +0800

    up

---
+++ VLCDemo/XTPlayer/XTPlayerVC.h
@@ -0,0 +1,13 @@
+//
+//  XTPlayerVC.h
+//  VLCDemo
+//
+//  Created by teason23 on 2018/2/26.
+//  Copyright © 2018年 teason23. All rights reserved.
+//
+
+#import "RootCtrl.h"
+
+@interface XTPlayerVC : RootCtrl
+
+@end
---
+++ VLCDemo/XTPlayer/XTPlayerVC.m
@@ -0,0 +1,37 @@
+//
+//  XTPlayerVC.m
+//  VLCDemo
+//
+//  Created by teason23 on 2018/2/26.
+//  Copyright © 2018年 teason23. All rights reserved.
+//
+
+#import "XTPlayerVC.h"
+
+@interface XTPlayerVC ()
+
+@end
+
+@implementation XTPlayerVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
